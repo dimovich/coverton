1906b16b1a9fdba0d680161aa81cba1fa64df95b
dimovich <dimov@dd.com>
2017-05-19 20:23:04 +0300

    ...

--- src/cljs/coverton/core.cljs
+++ src/cljs/coverton/core.cljs
@@ -10,52 +10,44 @@
   (vec (concat (subvec coll 0 pos) (subvec coll (inc pos)))))
 
 
-(defn dom-pos [el]
+(defn get-xy [el]
   [(-> (sel1 el) .getBoundingClientRect .-left)
    (-> (sel1 el) .getBoundingClientRect .-top)])
 
 
-(defn add-label [labels]
-  (let [c (count @labels)]
-    (swap! labels conj {:text "Hello"})))
+(defn autosize-input [{:keys [x y parent uuid] :as opts}]
+  (let [v (r/atom (assoc opts :text ""))]
+    (r/create-class
+     {:reagent-render
+      (fn [{:keys [x y parent uuid] :as opts}]
+        ^{:key (:id uuid)}
+        [:div
+         [autosize/input {:value (:text @v)
+                          :on-change (fn [e] (swap! v assoc :text (.. e -target -value)))
+                          :class :editor-label
+                          :style {:left x
+                                  :top y}}]])
+      ;; focus
+      :component-did-mount
+      (fn [self _] (-> self r/dom-node .-firstChild .focus))})))
 
 
 (defn editor []
-  (let [labels (r/atom [])
-        focus (r/atom false)]
-    (r/create-class
-     {:display-name "cover-editor"
-      :reagent-render
-      (fn []
-        [:div.editor
-         [:img.editor-image {:src "assets/img/coverton.jpg"
-                             :on-click (fn [e]
-                                         (let [[px py] (dom-pos :.editor)]
-                                           (swap! labels conj {:text ""
-                                                               :x (- (.-clientX e) px)
-                                                               :y (- (.-clientY e) py)}))
-                                         (reset! focus true))}]
-         ;; labels
-         (doall
-          (map-indexed
-           (fn [idx item]
-             (let [v (r/cursor labels [idx :text])]
-               ^{:key (str "label" idx)}
-               [autosize/input {:value @v
-                                :on-change (fn [e] (reset! v (.. e -target -value)))
-                                :on-blur (fn [e] (when (empty? @v)
-                                                   (swap! labels vec-remove idx)))
-                                :class :editor-label
-                                :id (str "label" idx)
-                                :style {:left (:x item)
-                                        :top (:y item)}}]))
-           @labels))])
-      :component-did-update
-      (fn []
-        (when @focus
-          (do
-            (.focus (sel1 (str "#label" (dec (count @labels)))))
-            (reset! focus false))))})))
+  (let [labels (r/atom {})]
+    (fn []
+      (into
+       [:div.editor
+        [:img.editor-image {:src "assets/img/coverton.jpg"
+                            :on-click (fn [e]
+                                        (let [[px py] (get-xy :.editor)
+                                              uuid (random-uuid)]
+                                          (swap! labels assoc
+                                                 uuid [autosize-input
+                                                       {:x (- (.-clientX e) px)
+                                                        :y (- (.-clientY e) py)
+                                                        :uuid uuid
+                                                        :parent labels}])))}]]
+       (vals @labels)))))
 
 
 
@@ -64,6 +56,3 @@
     (do
       (r/render [editor] (sel1 :.app)))))
 
-
-
-;; FIXME: input cursor jumps to the end
