4af2d5b368df5b431e987aa0f8f90bae82503161
dimovich <dimovich@gmail.com>
2017-10-20 16:21:03 +0300

    ...

--- dump.clj
+++ dump.clj
@@ -196,3 +196,76 @@ f
   (swap! boot.repl/*default-middleware*
          concat '[cider.nrepl/cider-middleware])
   identity)
+
+
+
+
+
+
+
+{:cover {:fabric [..]}}
+
+:component-will-unmount
+
+
+
+(.toSVG @canvas (clj->js {:suppressPreamble true}))
+
+
+
+(def svg "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" version=\"1.1\" width=\"475\" height=\"475\" xml:space=\"preserve\"><desc>Created with Fabric.js 1.5.0</desc><defs></defs><g transform=\"translate(237.01 237.01) scale(0.99 0.99)\">\n<image xlink:href=\"http://localhost:5000/assets/img/coverton.jpg\" x=\"-240\" y=\"-240\" style=\"stroke: none; stroke-width: 1; stroke-dasharray: ; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,0,0); fill-rule: nonzero; opacity: 1;\" width=\"480\" height=\"480\" preserveAspectRatio=\"none\"></image>\n</g>\n\t<g transform=\"translate(111.5 103.47)\">\n\t\t<text font-family=\"GothaPro\" font-size=\"61.75\" font-weight=\"normal\" style=\"stroke: none; stroke-width: 1; stroke-dasharray: ; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: #FF9933; fill-rule: nonzero; opacity: 1;\" ><tspan x=\"-73.4824\" y=\"13.8691\" fill=\"#FF9933\">hello</tspan></text>\n\t</g>\n\t<g transform=\"translate(420.5 145.47)\">\n\t\t<text font-family=\"GothaPro\" font-size=\"61.75\" font-weight=\"normal\" style=\"stroke: none; stroke-width: 1; stroke-dasharray: ; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: #FF9933; fill-rule: nonzero; opacity: 1;\" ><tspan x=\"-73.4824\" y=\"13.8691\" fill=\"#FF9933\">hello</tspan></text>\n\t</g>\n\t<g transform=\"translate(135.5 415.47)\">\n\t\t<text font-family=\"GothaPro\" font-size=\"61.75\" font-weight=\"normal\" style=\"stroke: none; stroke-width: 1; stroke-dasharray: ; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: #FF9933; fill-rule: nonzero; opacity: 1;\" ><tspan x=\"-73.4824\" y=\"13.8691\" fill=\"#FF9933\">hello</tspan></text>\n\t</g>\n</svg>")
+
+
+(loadSVGFromString
+ svg
+ (fn [objs opts]
+   (map #(.add @canvas %) objs)
+   #_(let [group (groupSVGElements objs opts)]
+       (info objs)
+       (.. @canvas
+           (add objs)
+           renderAll))))
+
+
+
+
+
+
+
+(loadSVGFromString
+ svg
+ (fn [objs opts]))
+
+
+
+
+#js {:objects #js [#js {:type "i-text", :originX "left", :originY "top", :left 45.02, :top 166, :width 93.12, :height 51.29, :fill "#FF9933", :stroke nil, :strokeWidth 1, :strokeDashArray nil, :strokeLineCap "butt", :strokeLineJoin "miter", :strokeMiterLimit 10, :scaleX 1, :scaleY 1, :angle 0, :flipX false, :flipY false, :opacity 1, :shadow nil, :visible true, :clipTo nil, :backgroundColor "", :fillRule "nonzero", :globalCompositeOperation "source-over", :text "hello", :fontSize 39.13, :fontWeight "normal", :fontFamily "GothaPro", :fontStyle "", :lineHeight 1.16, :textDecoration "", :textAlign "left", :textBackgroundColor "", :styles #js {}} #js {:type "i-text", :originX "left", :originY "top", :left 63.02, :top 87, :width 93.12, :height 51.29, :fill "#FF9933", :stroke nil, :strokeWidth 1, :strokeDashArray nil, :strokeLineCap "butt", :strokeLineJoin "miter", :strokeMiterLimit 10, :scaleX 1, :scaleY 1, :angle 0, :flipX false, :flipY false, :opacity 1, :shadow nil, :visible true, :clipTo nil, :backgroundColor "", :fillRule "nonzero", :globalCompositeOperation "source-over", :text "hello", :fontSize 39.13, :fontWeight "normal", :fontFamily "GothaPro", :fontStyle "", :lineHeight 1.16, :textDecoration "", :textAlign "left", :textBackgroundColor "", :styles #js {}} #js {:type "i-text", :originX "left", :originY "top", :left 258.02, :top 54, :width 93.12, :height 51.29, :fill "#FF9933", :stroke nil, :strokeWidth 1, :strokeDashArray nil, :strokeLineCap "butt", :strokeLineJoin "miter", :strokeMiterLimit 10, :scaleX 1, :scaleY 1, :angle 0, :flipX false, :flipY false, :opacity 1, :shadow nil, :visible true, :clipTo nil, :backgroundColor "", :fillRule "nonzero", :globalCompositeOperation "source-over", :text "hello", :fontSize 39.13, :fontWeight "normal", :fontFamily "GothaPro", :fontStyle "", :lineHeight 1.16, :textDecoration "", :textAlign "left", :textBackgroundColor "", :styles #js {}}], :background "", :backgroundImage #js {:type "image", :originX "left", :originY "top", :left 0, :top 0, :width 480, :height 480, :fill "rgb(0,0,0)", :stroke nil, :strokeWidth 1, :strokeDashArray nil, :strokeLineCap "butt", :strokeLineJoin "miter", :strokeMiterLimit 10, :scaleX 0.63, :scaleY 0.63, :angle 0, :flipX false, :flipY false, :opacity 1, :shadow nil, :visible true, :clipTo nil, :backgroundColor "", :fillRule "nonzero", :globalCompositeOperation "source-over", :src "http://localhost:5000/assets/img/coverton.jpg", :filters #js [], :crossOrigin "", :alignX "none", :alignY "none", :meetOrSlice "meet"}}
+
+
+
+
+(loadSVGFromString
+ svg
+ (fn [objs opts]
+   (enlivenObjects objs
+                   (fn [objs]
+                     (doseq [o objs] (info o) #_(.add @canvas o))))))
+
+
+
+
+(loadSVGFromString
+ svg
+ (fn [objs opts]
+   (parseElements objs
+                  (fn [objs]
+                    (info objs)))))
+
+
+
+
+(loadSVGFromString
+ svg
+ (fn [objs opts]
+   (doseq [o objs]
+     (.add @canvas o))))
--- src/cljs/coverton/fabric/views.cljs
+++ src/cljs/coverton/fabric/views.cljs
@@ -1,11 +1,11 @@
 (ns coverton.fabric.views
-  (:require ;;[react-fabricjs]
-            [cljsjs.fabric]
-            [reagent.core       :as r]
+  (:require [reagent.core       :as r]
             [coverton.ed.subs   :as ed-sub]
             [coverton.ed.events :as ed-evt]
-            [re-frame.core      :refer [subscribe]]
-            [taoensso.timbre    :refer-macros [info]]))
+            [re-frame.core      :refer [subscribe dispatch]]
+            [taoensso.timbre    :refer-macros [info]]
+            [coverton.fonts     :refer [default-font]]
+            [cljsjs.fabric]))
 
 
 
@@ -13,74 +13,73 @@
 (def Rect    window.fabric.Rect)
 (def Text    window.fabric.Text)
 (def IText   window.fabric.IText)
-(def fromURL window.fabric.Image.fromURL)
+(def fromURL  window.fabric.Image.fromURL)
+(def loadSVGFromString window.fabric.loadSVGFromString)
+(def groupSVGElements window.fabric.util.groupSVGElements)
+(def enlivenObjects window.fabric.util.enlivenObjects)
+(def parseElements window.fabric.parseElements)
 
-;;(def fromURL (goog.object/getValueByKeys js/window "fabric" "Image" "fromURL"))
 
-
-(def canvas (r/atom nil))
+(def canvas (atom nil))
 
 
 (defn click->relative [e]
   (let [bounds (.. e -target getBoundingClientRect)
         x (- (.. e -clientX) (.. bounds -left))
         y (- (.. e -clientY) (.. bounds -top))]
-    [(/ x (.. bounds -width))
-     (/ y (.. bounds -height))]))
+    [x y]
+    #_[(/ x (.. bounds -width))
+       (/ y (.. bounds -height))]))
 
 
 
-(defn on-click-add-mark [evt]
-  (-> (.. evt -e)
-      click->relative
-      ed-evt/add-mark))
+(defn add-mark [canvas [x y]]
+  (let [mark (IText. "hello"
+                     (clj->js {:left x :top y
+                               :fill (:color default-font) :cursorColor (:color default-font)
+                               :fontFamily (:font-family default-font) :fontSize 40}))]
+    (.add canvas mark)))
 
 
-()
 
-(defn marks->fabric [canvas marks]
-  (doall
-   (map (fn [[id mark]]
-          (let [width (.. canvas -width)
-                height (.. canvas -height)
-                [x y] (:pos mark)
-                x (* x width)
-                y (* y height)
-                text (or (:text mark) "hello")
-                text (IText. text (clj->js {:left x :top y}))
-                text (.on text "mousedown" #(do (info (.. % -e -target -width))
-                                                (.. % -e preventDefault)))]
-            (.add canvas text)))
-        marks)))
-
+(defn on-click-add-mark [canvas evt]
+  (->> (.. evt -e)
+       click->relative
+       (add-mark canvas)))
 
 
 
 (defn cover->fabric [canvas cover]
-  (.clear canvas)
-  (let [url (:cover/image-url cover)
-        marks (:cover/marks cover)]
-    ;; background
-    (fromURL url (fn [img]
-                   ;;(.on img "changed" #(info "selected"))
-                   (.scaleToWidth img (.. canvas -width))
-                   (.setBackgroundImage canvas img)
-                   (.renderAll canvas)))
-    ;; marks
-    (marks->fabric canvas marks)))
+  (if-let [fabric-json (:cover/fabric-json cover)]
+    (do ;;init from object
+      (info fabric-json)
+      (.loadFromJSON canvas fabric-json (fn [objs] (info "loaded" objs))))
+    ;;create new
+    (let [url (:cover/image-url cover)]
+      ;;background
+      (fromURL url
+               (fn [img]
+                 (.scaleToWidth img (.. canvas -width))
+                 (.setBackgroundImage canvas img
+                                      (.bind canvas.renderAll canvas)))))))
 
 
 
 
 (defn init-fabric [canvas]
-  (.on canvas "mouse:down" (fn [evt]
-                             (info (js->clj (.. evt -e -target -className)))
-                             (on-click-add-mark evt))))
+  (.on canvas "mouse:down"
+       (fn [evt]
+         ;; when clicking some empty space
+         (when-not (.. evt -target)
+           (on-click-add-mark canvas evt)))))
+
 
 
 
 (defn fabric [cover]
-  (let [_ (ed-evt/initialize cover)
+  (let [_     (ed-evt/initialize cover)
+        cover (subscribe [::ed-sub/cover])
+        fabric->json (fn [] (.toJSON @canvas))
         update-size (fn [canvas parent]
                       (.setWidth canvas (.. parent -clientHeight))
                       (.setHeight canvas (.. parent -clientHeight)))]
@@ -93,16 +92,24 @@
           (reset! canvas c)
           (init-fabric   @canvas)
           (update-size   @canvas (r/dom-node this))
-          (cover->fabric @canvas (r/props this))))
+          (cover->fabric @canvas @cover)))
       
       :component-did-update
       (fn [this]
-        (cover->fabric @canvas (r/props this)))
+        (info "updated" (.toJSON @canvas))
+        (cover->fabric @canvas @cover))
+
+      :component-will-unmount
+      (fn [this]
+        (info "unmounting")
+        ;;(info {:cover/fabric-json (fabric->json)})
+        )
       
       :reagent-render
-      (fn [cover]
+      (fn []
         [:div.editor-img
-         [:canvas#canv]])})))
+         [:canvas#canv]
+         #_[:div (str @cover)]])})))
 
 
 
@@ -111,3 +118,4 @@
 ;; TODO:
 ;; on-click-add-mark
 ;; save state
+
